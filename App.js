26ea0d74801684331b15f28e7dc278e1da384ff6
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-07-22 11:35:15 +0700

    add react navigation

--- App.js
+++ App.js
@@ -6,7 +6,48 @@
  * @flow strict-local
  */
 
-// import React from 'react';
-import HomeScreen from './src/screen/HomeScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator, CardStyleInterpolators } from "@react-navigation/stack";
 
-export default HomeScreen;
+import HomeScreen from "./src/screen/HomeScreen";
+import MovieDetailScreen from "./src/screen/MovieDetailScreen";
+
+const Stack = createStackNavigator();
+
+const HomeNavigator = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: false,
+          headerTransparent: true,
+        }}
+      />
+      <Stack.Screen
+        name="MovieDetail"
+        component={MovieDetailScreen}
+        options={{
+          gestureEnabled: false,
+          cardStyleInterpolator: CardStyleInterpolators.forRevealFromBottomAndroid,
+          headerTransparent: true,
+          headerTitle: false,
+          headerBackTitleVisible: false,
+          headerTintColor: "#ffffff",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <HomeNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
