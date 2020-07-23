78d630b85d71eb7ebf717cac169f187a6f3a29be
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2020-07-23 11:58:35 +0700

    add color helper

---
+++ src/helper/Color.js
@@ -0,0 +1,18 @@
+export const white = "#ffffff";
+export const black = "#000000";
+export const orange = "#fc6b02";
+export const yellow = "#ffd700";
+
+export const gray = "#e9e9e9";
+export const lightGray = "#f0f0f0";
+export const darkGray = "#a1a1a4";
+
+export const transparent = "transparent";
+
+export const freeze = "#f6f6f6";
+export const blue = "#8190a5";
+export const darkBlue = "#47525e";
+export const pink = "#f95f62";
+export const lightRed = "#ff7f7f";
+export const lightYellow = "#eab079";
+export const lightGreen = "#82c596";
